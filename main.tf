ce092776fd5486a2695df67610b4070bbb81068c
MagenX <magenx@users.noreply.github.com>
2023-06-21 09:06:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,7 +114,7 @@ resource "hcloud_server" "this" {
     network_id = hcloud_network.this.id
     ip         = hcloud_network.this.ip_range
   }
-  user_data = cloudinit_config.this.rendered[each.key]
+  user_data = data.cloudinit_config.this.rendered[each.key]
 }
 
 ## Server configuration cloud-init
