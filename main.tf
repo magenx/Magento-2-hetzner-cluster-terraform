810bf428d16949fdf5c907415e49b89f9c646599
MagenX <magenx@users.noreply.github.com>
2023-06-23 12:18:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "hcloud_load_balancer" "this" {
 resource "hcloud_load_balancer_network" "this" {
   load_balancer_id = hcloud_load_balancer.this.id
   network_id       = hcloud_network.this.id
-  ip               = 
+  #ip               = 
   enable_public_interface = false
 }
 
